ced81f73563250bec6272bb996ce5b552e687c40
donnalynnn <donnalyn.dev@gmail.com>
2024-07-15 11:11:30 +0800

    rainbowww

--- lib/Screens/home/homepage.dart
+++ lib/Screens/home/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:speakbright_mobile/Widgets/colors.dart';
+import 'dart:math';
 
 class DashBoard extends StatefulWidget {
   const DashBoard({super.key});
@@ -59,6 +60,18 @@ class _DashBoardState extends State<DashBoard> {
   @override
   Widget build(BuildContext context) {
     List<String> words = ["Charger", "Pencil", "Table", "Dice"];
+     
+      List<Color> boxcolors = [
+      Colors.red,
+      Colors.orange,
+      const Color.fromARGB(255, 237, 195, 7),
+      Colors.green,
+      Colors.blue,
+      Colors.indigo,
+      Colors.purple,
+    ];
+
+    List<double> containerHeights = List.generate(7, (index) => index*50 * 0.5 + 1);
 
     return Scaffold(
         // appBar: AppBar(
@@ -66,98 +79,130 @@ class _DashBoardState extends State<DashBoard> {
         // ),
         backgroundColor: kwhite,
         body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                flex: 1,
-                child:Container(
-                  decoration: const BoxDecoration(
-                    color: mainpurple,
-                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(80))
-                  ),
+          child: Column(children: [
+            // Expanded(
+            //   flex: 1,
+            //   child: Container(
+            //     decoration: const BoxDecoration(
+            //         color: mainpurple,
+            //         borderRadius:
+            //             BorderRadius.only(bottomLeft: Radius.circular(60))),
+            //   ),
+            // ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white, // Initial container is white
+                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(60))),
+              ),
+            ),
+            ...List.generate(6, (index) { // Generate the rainbow containers
+              double height = containerHeights[index + 1]; // Skip the first index for the initial white container
+              Color color = boxcolors[index]; // Cycle through colors starting from red
+              return Container(
+                height: height,
+                decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(60)),
                 ),
+              );
+            }),
+            const SizedBox(
+              height: 10,
+            ),
+            //cards area
+            Expanded(
+              flex: 5,
+              child: GridView.builder(
+                
+                
+                padding: const EdgeInsets.all(16.0),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 25.0,
+                  mainAxisSpacing: 25.0,
                 ),
-                const SizedBox(height: 10,),
-                //cards area
-              Expanded(
-                flex: 5,
-                child: GridView.builder(
-                  padding: const EdgeInsets.all(16.0),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 18.0,
-                    mainAxisSpacing: 18.0,
-                  ),
-                  itemCount: words.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        _speak(words[index]);
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: mainpurple,
-                          borderRadius: BorderRadius.circular(20),
-                          // boxShadow: [
-                          //   BoxShadow(
-                          //     color: Colors.black.withOpacity(0.4), 
-                          //     spreadRadius: 3, 
-                          //     blurRadius: 10, 
-                          //     offset: const Offset(0, 3), 
-                          //   ),
-                          // ],
-                        ),
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Container(
-                                  width: 50,
-                                  height: 50,
-                                  decoration: const BoxDecoration(
-                                    color: kwhite,
-                                    borderRadius: BorderRadius.only(topLeft: Radius.circular(20), bottomRight: Radius.circular(100),),
+                itemCount: words.length,
+                itemBuilder: (context, index) {
+                  int colorIndex = index % boxcolors.length;
+                  return GestureDetector(
+                    onTap: () {
+                      _speak(words[index]);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: kwhite,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            spreadRadius: 2,
+                            blurRadius: 1,
+                            offset: const Offset(2, 2),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                width: 50,
+                                height: 50,
+                                decoration: const BoxDecoration(
+                                  color: Colors.transparent,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(20),
+                                    bottomRight: Radius.circular(100),
                                   ),
-                                  child: const Icon(Icons.music_note, color: mainpurple, size: 30,),
-                                )
-                              ],
-                            ),
-                            const SizedBox(height: 30,),
-                            Center(
-                              child: Text(
-                                words[index],
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
                                 ),
+                                child: Icon(
+                                  Icons.music_note,
+                                  color: boxcolors[colorIndex],
+                                  size: 40,
+                                ),
+                              )
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          Center(
+                            child: Text(
+                              words[index],
+                              style: TextStyle(
+                                color:boxcolors[colorIndex],
+                                fontSize: 20,
                               ),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    );
-                  },
-                ),
+                    ),
+                  );
+                },
               ),
+            ),
 
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: DropdownButton<String>(
-                  value: selectedVoice,
-                  items: voices.map<DropdownMenuItem<String>>((dynamic voice) {
-                    return DropdownMenuItem<String>(
-                      value: voice["name"],
-                      child: Text(voice["name"]),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      selectedVoice = newValue!;
-                    });
-                  },
-                  isExpanded: true,
-                ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: DropdownButton<String>(
+                value: selectedVoice,
+                items: voices.map<DropdownMenuItem<String>>((dynamic voice) {
+                  return DropdownMenuItem<String>(
+                    value: voice["name"],
+                    child: Text(voice["name"]),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedVoice = newValue!;
+                  });
+                },
+                isExpanded: true,
               ),
+            ),
           ]),
         ));
   }
