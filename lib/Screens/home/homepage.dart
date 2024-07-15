83e5b51677a2d1109344a7615f1bb60941217638
donnalynnn <donnalyn.dev@gmail.com>
2024-07-15 08:47:11 +0800

    modified ui for homepage

--- lib/Screens/home/homepage.dart
+++ lib/Screens/home/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:speakbright_mobile/Widgets/colors.dart';
 
 class DashBoard extends StatefulWidget {
   const DashBoard({super.key});
@@ -60,64 +61,104 @@ class _DashBoardState extends State<DashBoard> {
     List<String> words = ["Charger", "Pencil", "Table", "Dice"];
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dashboard'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: GridView.builder(
-              padding: const EdgeInsets.all(16.0),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16.0,
-                mainAxisSpacing: 16.0,
-              ),
-              itemCount: words.length,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    _speak(words[index]);
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Center(
-                      child: Text(
-                        words[index],
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
+        // appBar: AppBar(
+        //   title: const Text('Dashboard'),
+        // ),
+        backgroundColor: kwhite,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child:Container(
+                  decoration: const BoxDecoration(
+                    color: mainpurple,
+                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(80))
+                  ),
+                ),
+                ),
+                const SizedBox(height: 10,),
+                //cards area
+              Expanded(
+                flex: 5,
+                child: GridView.builder(
+                  padding: const EdgeInsets.all(16.0),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 18.0,
+                    mainAxisSpacing: 18.0,
+                  ),
+                  itemCount: words.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        _speak(words[index]);
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: mainpurple,
+                          borderRadius: BorderRadius.circular(20),
+                          // boxShadow: [
+                          //   BoxShadow(
+                          //     color: Colors.black.withOpacity(0.4), 
+                          //     spreadRadius: 3, 
+                          //     blurRadius: 10, 
+                          //     offset: const Offset(0, 3), 
+                          //   ),
+                          // ],
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  width: 50,
+                                  height: 50,
+                                  decoration: const BoxDecoration(
+                                    color: kwhite,
+                                    borderRadius: BorderRadius.only(topLeft: Radius.circular(20), bottomRight: Radius.circular(100),),
+                                  ),
+                                  child: const Icon(Icons.music_note, color: mainpurple, size: 30,),
+                                )
+                              ],
+                            ),
+                            const SizedBox(height: 30,),
+                            Center(
+                              child: Text(
+                                words[index],
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: DropdownButton<String>(
-              value: selectedVoice,
-              items: voices.map<DropdownMenuItem<String>>((dynamic voice) {
-                return DropdownMenuItem<String>(
-                  value: voice["name"],
-                  child: Text(voice["name"]),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedVoice = newValue!;
-                });
-              },
-              isExpanded: true,
-            ),
-          ),
-        ],
-      ),
-    );
+                    );
+                  },
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: DropdownButton<String>(
+                  value: selectedVoice,
+                  items: voices.map<DropdownMenuItem<String>>((dynamic voice) {
+                    return DropdownMenuItem<String>(
+                      value: voice["name"],
+                      child: Text(voice["name"]),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedVoice = newValue!;
+                    });
+                  },
+                  isExpanded: true,
+                ),
+              ),
+          ]),
+        ));
   }
 }
