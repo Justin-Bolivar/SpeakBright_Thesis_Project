2c9003359314a0c4f79830d1488636ee1443c58b
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2024-08-06 14:44:08 +0800

    removed old header and changed to new header

--- lib/Screens/home/header_container.dart
+++ lib/Screens/home/header_container.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:speakbright_mobile/Screens/home/profile_dialogue.dart';
 import 'package:speakbright_mobile/Widgets/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
