b3dce7239b7b66ec4e178fb2c0555902e90f2309
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2024-08-09 15:00:00 +0800

    seperated functions to different files to reuse

---
+++ lib/Widgets/services/tts_service.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+class TTSService {
+  final FlutterTts _flutterTts = FlutterTts();
+
+  TTSService() {
+    _setupTTS();
+  }
+
+  Future<void> _setupTTS() async {
+    await _flutterTts.setLanguage("en-US");
+    await setDefaultVoice();
+  }
+
+  Future<void> setDefaultVoice() async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+    String voiceName = connectivityResult != ConnectivityResult.none
+        ? "Microsoft Aria Online (Natural) - English (United States)"
+        : "Microsoft Zira - English (United States)";
+
+    await _flutterTts.setVoice({"name": voiceName, "locale": "en-US"});
+    await _flutterTts.setPitch(1.0);
+  }
+
+  Future<void> speak(String text) async {
+    await setDefaultVoice();
+    await _flutterTts.speak(text);
+  }
+
+  void stop() {
+    _flutterTts.stop();
+  }
+}
