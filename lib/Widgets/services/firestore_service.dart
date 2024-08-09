6ff59e2cc599277e8d7a25416d8b8da61d584e4f
Donnalyn <donnalynrrma@gmail.com>
2024-08-09 20:16:24 +0800

    add tap cards record to firestore

--- lib/Widgets/services/firestore_service.dart
+++ lib/Widgets/services/firestore_service.dart
@@ -31,4 +31,48 @@ class FirestoreService {
       throw Exception('Error fetching categories: $e');
     }
   }
+
+  //record for the tapped cards
+  Future<void> storeTappedCards(String cardTitle, String category) async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      throw Exception('No user is currently signed in.');
+    }
+
+    final CollectionReference tappedRef =
+        FirebaseFirestore.instance.collection('tappedCardsPerSession');
+    final DateTime now = DateTime.now();
+
+    //latest session within the last 5 minutes
+    final QuerySnapshot querySnapshot = await tappedRef
+        .where('userID', isEqualTo: user.uid)
+        .where('sessionTime',
+            isGreaterThan:
+                Timestamp.fromDate(now.subtract(const Duration(minutes: 5))))
+        .orderBy('sessionTime', descending: true)
+        .limit(1)
+        .get();
+
+    DocumentReference sessionDoc;
+
+    if (querySnapshot.docs.isNotEmpty) {
+      // recent session exists, use it
+      sessionDoc = querySnapshot.docs.first.reference;
+    } else {
+      // if no recent session found
+      final Map<String, dynamic> newSessionData = {
+        'userID': user.uid,
+        'sessionID': FirebaseFirestore.instance.collection('sessions').doc().id,
+        'sessionTime': Timestamp.fromDate(now),
+      };
+      sessionDoc = await tappedRef.add(newSessionData);
+    }
+
+    // subcollection
+    await sessionDoc.collection('cards').add({
+      'cardTitle': cardTitle,
+      'category': category,
+      'tapTime': Timestamp.fromDate(now),
+    });
+  }
 }
