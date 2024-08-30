b25994429321125493cc76ee53685fa494dbf193
Donnalyn <donnalynrrma@gmail.com>
2024-08-30 11:04:27 +0800

    explore page with recommendation system (v1)

--- lib/Widgets/cards/explore_card_grid.dart
+++ lib/Widgets/cards/explore_card_grid.dart
@@ -1,17 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:speakbright_mobile/Widgets/cards/card_model.dart';
-
 import 'explore_card_item.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class ExploreCardGrid extends StatelessWidget {
+// Keep this function as is
+Future<List<dynamic>> fetchRecommendations(String userId) async {
+  final url = Uri.parse('http://localhost:8000/recommendations/$userId');
+  try {
+    final response = await http.post(url);
+    
+    if (response.statusCode == 200) {
+      return json.decode(response.body)['recommendations'];
+    } else {
+      throw Exception('Failed to load recommendations');
+    }
+  } catch (e) {
+    print(e.toString());
+    rethrow;
+  }
+}
+
+class ExploreCardGrid extends StatefulWidget {
+  final String userId;
   final List<CardModel> cards;
   final Function(String) onCardTap;
 
   const ExploreCardGrid({
     super.key,
+    required this.userId,
     required this.cards,
     required this.onCardTap,
   });
+
+  @override
+  State<ExploreCardGrid> createState() => _ExploreCardGridState();
+}
+
+class _ExploreCardGridState extends State<ExploreCardGrid> {
+  List<dynamic> recommendations = [];
+  bool isLoading = false;
+
+  void fetchAndFilterRecommendations() async {
+  setState(() {
+    isLoading = true;
+  });
+
+  try {
+    Map<String, dynamic> response = (await fetchRecommendations(widget.userId)) as Map<String, dynamic>;
+
+    List<dynamic> recommendations = response['recommendations']
+        .map((doc) => CardModel.fromFirestore(doc))
+          .toList();
+
+    List<CardModel> nonRecommendedCards = widget.cards.where((card) => !recommendations.contains(card)).toList();
+
+    setState(() {
+      recommendations.addAll(nonRecommendedCards);
+      isLoading = false;
+    });
+  } catch (e) {
+    print('Error fetching recommendations: $e');
+    setState(() {
+      isLoading = false;
+    });
+  }
+}
+  @override
+  void initState() {
+    super.initState();
+    fetchAndFilterRecommendations();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +81,14 @@ class ExploreCardGrid extends StatelessWidget {
         crossAxisSpacing: 16.0,
         mainAxisSpacing: 16.0,
       ),
-      itemCount: cards.length,
+      itemCount: recommendations.length,
       itemBuilder: (context, index) {
+        CardModel card = recommendations[index];
+
         return ExploreCardItem(
-          card: cards[index],
-          colorIndex: index,
-          onTap: () => onCardTap(cards[index].title),
+          card: card,
+          colorIndex: recommendations[index],
+          onTap: () => widget.onCardTap(card.title),
         );
       },
     );
