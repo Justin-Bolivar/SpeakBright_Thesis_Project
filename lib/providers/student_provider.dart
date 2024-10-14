7f5c227a2b097dc47d42a2b90ca853d6b9f27225
Donnalyn <donnalynrrma@gmail.com>
2024-10-14 10:04:11 +0800

    student readiness deployed onrender

--- lib/providers/student_provider.dart
+++ lib/providers/student_provider.dart
@@ -25,35 +25,44 @@ class PhaseNotifier extends StateNotifier<int> {
 
 final currentUserPhaseProvider = StateProvider<int>((ref) => 1);
 
-
-
 final freqProvider = StateProvider<List<double>?>((ref) => []);
+
 class FrequencyProvider extends ChangeNotifier {
   List<double>? _frequencies;
   String studentID;
-  
+
   var ref;
 
   FrequencyProvider(this.ref) : studentID = ref.watch(studentIdProvider);
 
-
   Future<List<double>> get frequencies async {
     if (_frequencies != null) return _frequencies!;
-    
 
     try {
-      final frequenciesMap = await FirestoreService().getPromptFrequencies(studentID);
-      
+      final frequenciesMap =
+          await FirestoreService().getPromptFrequencies(studentID);
+
       List<double> frequenciesList = frequenciesMap.entries.map((entry) {
-        return ((entry.value / frequenciesMap.values.reduce((a, b) => a + b)) * 100).roundToDouble();
+        int value = entry.value;
+        if (value == 0) {
+          return 0.0;
+        }
+        return ((value / frequenciesMap.values.reduce((a, b) => a + b)) * 100)
+            .roundToDouble();
       }).toList();
 
+      // Ensure the list has at least one element
+      if (frequenciesList.isEmpty) {
+        frequenciesList.add(0.0);
+      }
+
       _frequencies = frequenciesList;
+      print(_frequencies);
       notifyListeners();
       return frequenciesList;
     } catch (e) {
       print('Error calculating frequencies: $e');
-      rethrow; 
+      rethrow;
     }
   }
 }
